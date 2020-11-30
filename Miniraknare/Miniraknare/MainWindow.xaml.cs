f5c514f760b0750ac955c1bba74ecb461885eed4
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2020-11-30 22:54:30 +0100

    miniräknare

--- Miniraknare/Miniraknare/MainWindow.xaml.cs
+++ Miniraknare/Miniraknare/MainWindow.xaml.cs
@@ -24,5 +24,95 @@ namespace Miniraknare
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                switch (button.Content)
+                {
+
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "+":
+                    case "-":
+                    case "/":
+                    case "*":
+
+                        textblock.Text += button.Content;
+                        break;
+                    case "=":
+                        textblock.Text += button.Content;
+                        if (textblock.Text.Contains('+'))
+                        {
+                            textblock.Text += calculation('+');
+                        }
+                        else if (textblock.Text.Contains('-'))
+                        {
+                            textblock.Text += calculation('-');
+                        }
+                        else if (textblock.Text.Contains('/'))
+                        {
+                            textblock.Text += calculation('/');
+                        }
+                        else if (textblock.Text.Contains('*'))
+                        {
+                            textblock.Text += calculation('*');
+                        }
+
+                        break;
+                    case "AC":
+                        textblock.Text = "";
+                        break;
+
+
+                }
+              
+               
+            }
+        }
+
+        public String calculation(char countingSign)
+        {
+            var listText = textblock.Text.Split('+', '-', '/', '*' );
+
+            var firstNumber = Convert.ToDouble(listText[0]);
+            var secondNuber = (listText[1]);
+
+            var result = 0.0;
+            switch (countingSign)
+            {
+                case '+':
+                    result = firstNumber + secondNuber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNuber;
+                    break;
+                case '/':
+                    result = firstNumber / secondNuber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNuber;
+                    break;
+
+
+
+            }
+            string resultString = Convert.ToString(result);
+            return resultString;
+        }
+        
+
+
+
+
     }
 }
